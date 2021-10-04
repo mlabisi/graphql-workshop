3d0f22aa904ad7d8edf4affe820a9926dd3fd069
sam hernandez <mhernansam@gmail.com>
2021-10-03 19:47:46 -0700

    updated schema & added offline sls mode

--- index.js
+++ index.js
@@ -6,44 +6,130 @@ const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
 
 const client = new DynamoDBClient({ region: "us-east-1" });
 
-const getBooks = async () => {
-  const params = {
-    TableName: "books",
-  };
+const fs = require('fs')
+const typeDefs = fs.readFileSync("./schema.gql").toString("utf-8");
 
-//const get
+const getAddresses = async () => {
+    const params = {
+        TableName: "addresses",
+    };
 
-  try {
-    const results = await client.send(new ScanCommand(params));
-    const books = [];
-    results.Items.forEach((item) => {
-      books.push(unmarshall(item));
-    });
-    return books;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
-};
+    try {
+          const results = await client.send(new ScanCommand(params));
+          const addresses = [];
+          results.Items.forEach((item) => {
+            books.push(unmarshall(item));
+          });
+          return addresses;
+        } catch (err) {
+          console.error(err);
+          return err;
+        }
+      };
 
-// schema
-const typeDefs = gql`
-  scalar JSON
-  type Book {
-    id: ID!
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
+const getMenuItems = async () => {
+    const params = {
+        TableName: "menuItems",
+    };
+
+    try {
+          const results = await client.send(new ScanCommand(params));
+          const menuItems = [];
+          results.Items.forEach((item) => {
+            books.push(unmarshall(item));
+          });
+          return menuItems;
+        } catch (err) {
+          console.error(err);
+          return err;
+        }
+      };
+
+const hoursItems = [
+    {
+        "id": 1,
+        "openTime": "9 AM",
+        "closeTime": "8 PM"
+    },
+    {
+        "id": 2,
+        "openTime": "6 AM",
+        "closeTime": "9 PM"
+    },
+    {
+        "id": 3,
+        "openTime": "7 AM",
+        "closeTime": "4 PM"
+    }
+];
+
+const shops = [
+    {
+        "id": 1,
+        "addressId": 1,
+        "menuItemIds": [2, 3],
+        "hoursIds": 1
+    },
+    {
+        "id": 2,
+        "addressId": 2,
+        "menuItemIds": [1, 3],
+        "hoursIds": 2
+    },
+    {
+        "id": 3,
+        "addressId": 3,
+        "menuItemIds": [1, 2],
+        "hoursIds": 3
+    }
+];
+
+const addresses = [
+    {
+        "id": 1,
+        "streetNumber": "123",
+        "streetName": "Java Way"
+    },
+    {
+        "id": 2,
+        "streetNumber": "456",
+        "streetName": "Mocha Ave"
+    },
+    {
+        "id": 3,
+        "streetNumber": "789",
+        "streetName": "Machiatto Lane"
+    },
+];
+
+const menuItems = [
+    {
+        "id": 1,
+        "name": "Peppermint Latte",
+        "price": 3.00
+    },
+    {
+        "id": 2,
+        "name": "Matcha Latte",
+        "price": 6.00
+    },
+    {
+        "id": 3,
+        "name": "Vanilla Latte",
+        "price": 4.00
+    },
+];
 
 // resolvers
 const resolvers = {
-  Query: {
-    books: () => {
-      return getBooks();
+  RootQuery: {
+    shops: () => {
+        return shops;
+//      return getAddresses();
+    },
+    items: () => {
+        return menuItems;
+//      return getMenuItems();
     },
   },
 };
@@ -54,7 +140,8 @@ const server = new ApolloServer({
     resolvers,
     playground: {
             endpoint: "/dev/graphql",
-    }
+    },
+    introspection: true,
 });
 
 const handler = server.createHandler({
