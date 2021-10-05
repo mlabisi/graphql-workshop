61d3dfb17f3bd41ec0ade206940eacc97d9a0fc4
mora <mora@themoralcode.org>
2021-10-05 11:11:41 -0700

    Tweaking code

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // needed to deploy locally
-//const { ApolloServer, gql } = require("apollo-server");
-//import { gql, useMutation } from '@apollo/client';
-const { ApolloServer, gql } = require("apollo-server-lambda");
-const { unmarshall } = require("@aws-sdk/util-dynamodb");
-const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const {ApolloServer, gql} = require("apollo-server");
+// const { ApolloServer, gql } = require("apollo-server-lambda");
+const {unmarshall} = require("@aws-sdk/util-dynamodb");
+const {DynamoDBClient, ScanCommand} = require("@aws-sdk/client-dynamodb");
 
-const client = new DynamoDBClient({ region: "us-east-1" });
+const client = new DynamoDBClient({region: "us-east-1"});
 
 const fs = require('fs')
 const typeDefs = fs.readFileSync("./schema.gql").toString("utf-8");
@@ -16,17 +15,17 @@ const getAddresses = async () => {
     };
 
     try {
-          const results = await client.send(new ScanCommand(params));
-          const addresses = [];
-          results.Items.forEach((item) => {
+        const results = await client.send(new ScanCommand(params));
+        const addresses = [];
+        results.Items.forEach((item) => {
             addresses.push(unmarshall(item));
-          });
-          return addresses;
-        } catch (err) {
-          console.error(err);
-          return err;
-        }
-      };
+        });
+        return addresses;
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
+};
 
 const getMenuItems = async () => {
     const params = {
@@ -34,17 +33,17 @@ const getMenuItems = async () => {
     };
 
     try {
-          const results = await client.send(new ScanCommand(params));
-          const menuItems = [];
-          results.Items.forEach((item) => {
+        const results = await client.send(new ScanCommand(params));
+        const menuItems = [];
+        results.Items.forEach((item) => {
             menuItems.push(unmarshall(item));
-          });
-          return menuItems;
-        } catch (err) {
-          console.error(err);
-          return err;
-        }
-      };
+        });
+        return menuItems;
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
+};
 
 const hoursItems = [
     {
@@ -69,19 +68,19 @@ const shops = [
         "id": 1,
         "addressId": 1,
         "menuItemIds": [2, 3],
-        "hoursIds": 1
+        "hoursId": 1
     },
     {
         "id": 2,
         "addressId": 2,
         "menuItemIds": [1, 3],
-        "hoursIds": 2
+        "hoursId": 2
     },
     {
         "id": 3,
         "addressId": 3,
         "menuItemIds": [1, 2],
-        "hoursIds": 3
+        "hoursId": 3
     }
 ];
 
@@ -123,112 +122,122 @@ const menuItems = [
 
 // resolvers
 const resolvers = {
-  RootQuery: {
-    shops: () => {
-        return shops;
-    },
-    items: () => {
-        return menuItems;
+    RootQuery: {
+        shops: () => {
+            return shops;
+        },
+        items: () => {
+            return menuItems;
 //      return getMenuItems();
-    },
-    item: (id) => {
-        return menuItems.find(item => {return item.id === id});
-    },
-    item: (name) => {
-            return menuItems.find(item => {return item.name === name});
+        },
+        item: (_, {name, id}) => {
+            if (name != null && id != null) {
+                return menuItems.find(item => {
+                    return item.name === name && item.id == id
+                });
+            } else if (name != null) {
+                return menuItems.find(item => {
+                    return item.name === name
+                });
+            } else if (id != null) {
+                return menuItems.find(item => {
+                    return item.id == id
+                });
+            }
+
         }
-  },
-
-  RootMutation: {
-    addMenuItem: async (_, { name, price }, { dataSources }) => {
-      // calling data store to save/create item
-      // return created object back
-      const item = { "id": menuItems.length+1,"name": name, "price": price };
-      menuItems.push(item);
-      return item;
     },
-    addShop: async (_, { streetNumber, streetName }, { dataSources }) => {
-        // create new address
-        const newAddress = {
-            "id": addresses.length+1,
-            "streetNumber": streetNumber,
-            "streetName": streetName
-        };
-        addresses.push(newAddress);
 
-        const newShop = {
-            "id": menuItems.length+1,
-            "addressId": newAddress.id,
-//            "menuItemIds": [],
-//            "hoursIds":
-        };
-        shops.push(newShop);
+    RootMutation: {
+        addMenuItem: async (_, {name, price}, {dataSources}) => {
+            // calling data store to save/create item
+            // return created object back
+            const item = {
+                "id": menuItems.length + 1,
+                "name": name,
+                "price": price
+            };
+            menuItems.push(item);
+            return item;
+        },
+        addShop: async (_, {streetNumber, streetName}, {dataSources}) => {
+            // create new address
+            const newAddress = {
+                "id": addresses.length + 1,
+                "streetNumber": streetNumber,
+                "streetName": streetName
+            };
+            addresses.push(newAddress);
 
-        return newShop;
-    }
-  },
+            const newShop = {
+                "id": shops.length + 1,
+                "addressId": newAddress.id,
+                "menuItemIds": [1, 2, 3],
+                "hoursId": 1
+            };
+            shops.push(newShop);
 
-  Shop: {
-          address(parent) {
-            return addresses.filter(addr => addr.id === parent.id)[0];
-          },
-          menu(parent) {
+            return newShop;
+        }
+    },
+
+    Shop: {
+        address(parent) {
+            return addresses.filter(address => address.id === parent.id)[0];
+        },
+        menu(parent) {
             return menuItems.filter(item => item.id === parent.id);
-          },
-          hours(parent) {
+        },
+        hours(parent) {
             return hoursItems.filter(item => item.id === parent.id);
-          }
-  },
-  Address: {
-    streetNumber(parent) {
-        return addresses.filter(addr => addr.id === parent.id)[0].streetNumber;
+        }
     },
-    streetName(parent) {
-        return addresses.filter(addr => addr.id === parent.id)[0].streetName;
-    }
-  },
-  HoursItem: {
-    openTime(parent) {
-        return hoursItems.filter(hoursItem => hoursItems.id === parent.id)[0].openTime;
+    Address: {
+        streetNumber(parent) {
+            return (parent[0] ?? parent).streetNumber;
+        },
+        streetName(parent) {
+            return (parent[0] ?? parent).streetName;
+        }
     },
-    closeTime(parent) {
-         return hoursItems.filter(hoursItem => hoursItems.id === parent.id)[0].closeTime;
-    }
-  },
-  MenuItem: {
-    name(parent) {
-        return menuItems.filter(item => item.id === parent.id)[0].name;
+    HoursItem: {
+        openTime(parent) {
+            return (parent[0] ?? parent).openTime;
+        },
+        closeTime(parent) {
+            return (parent[0] ?? parent).closeTime;
+        }
     },
-    price(parent) {
-        return menuItems.filter(item => item.id === parent.id)[0].price;
+    MenuItem: {
+        name(parent) {
+            return (parent[0] ?? parent).name;
+        },
+        price(parent) {
+            return (parent[0] ?? parent).price;
+        }
     }
-  }
 };
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     playground: {
-            endpoint: "/dev/graphql",
+        endpoint: "/dev/graphql",
     },
     introspection: true,
 });
 
-const handler = server.createHandler({
-  cors: {
-    origin: '*',
-    credentials: true,
-  },
-});
-
-exports.graphqlHandler = handler;
-
-// to deploy locally
-//const server = new ApolloServer({
-//  typeDefs,
-//  resolvers,
-//});
+// SERVERLESS
+// const handler = server.createHandler({
+//   cors: {
+//     origin: '*',
+//     credentials: true,
+//   },
+// });
 //
-//server.listen().then(({ url }) => {
-//  console.log(`🚀  Server ready at ${url}`);
-//});
+// exports.graphqlHandler = handler;
+
+// LOCAL
+server.listen().then(({url}) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
