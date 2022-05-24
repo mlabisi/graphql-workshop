efd3e0a2ba9588bd2b8b41213b33d7b2a6feac5c
sam hernandez <mhernansam@gmail.com>
2022-05-24 11:00:33 -0700

    updaitng data to mad scientist + reorg

---
+++ data/hoursItems.js
@@ -0,0 +1,20 @@
+const hoursItems = [{
+        "id": 1,
+        "openTime": "9 AM",
+        "closeTime": "8 PM"
+    },
+    {
+        "id": 2,
+        "openTime": "6 AM",
+        "closeTime": "9 PM"
+    },
+    {
+        "id": 3,
+        "openTime": "7 AM",
+        "closeTime": "4 PM"
+    }
+];
+
+module.exports = {
+  hoursItems,
+};
---
+++ data/menuItems.js
@@ -0,0 +1,205 @@
+const menuItems = [{
+        "id": 1,
+        "name": "Mom's Old Fashioned Robot Nitro Cold Brew Oil",
+        "price": 3.00
+    },
+    {
+        "id": 2,
+        "name": "Bender's Espresso",
+        "price": 6.00
+    },
+    {
+        "id": 3,
+        "name": "Sprinkled Slurm Frappe",
+        "price": 4.00
+    },
+    {
+        "id": 4,
+        "name": "JoJo Slow Pour Pow",
+        "price": 3.65
+    },
+    {
+        "id": 5,
+        "name": "Mad Monkey Mocha",
+        "price": 4.30
+    },
+    {
+        "id": 6,
+        "name": "Puff Puff Pow Chai Tea",
+        "price": 2.30
+    },
+    {
+        "id": 7,
+        "name": "GLAYVIN! Guava Glow Joe ",
+        "price": 9.45
+    },
+    {
+        "id": 8,
+        "name": "mm-haiai Mint Mocha",
+        "price": 2.30
+    },
+    {
+        "id": 9,
+        "name": "Springfield Hot Shots",
+        "price": 7.40
+    },
+    {
+        "id": 10,
+        "name": "Cosmos Cappuccino",
+        "price": 3.80
+    },
+    {
+        "id": 11,
+        "name": "DeathRay Decaf",
+        "price": 3.80
+    },
+    {
+        "id": 12,
+        "name": "Destruction Ristretto",
+        "price": 2.30
+    },
+    {
+        "id": 13,
+        "name": "Mad Red Eye",
+        "price": 2.33
+    },
+    {
+        "id": 14,
+        "name": "Cage Concoction",
+        "price": 3.33
+    },
+    {
+        "id": 15,
+        "name": "Bernard's Brew",
+        "price": 2.33
+    },
+    {
+        "id": 16,
+        "name": "Mad Mind Mocha ",
+        "price": 2.01
+    },
+    {
+        "id": 17,
+        "name": "Villinous Mystery Venti",
+        "price": 2.81
+    },
+    {
+        "id": 18,
+        "name": "Copper Cola",
+        "price": 3.33
+    },
+    {
+        "id": 19,
+        "name": "Cookie Robot Crumble",
+        "price": 2.33
+    },
+    {
+        "id": 20,
+        "name": "Fart Gun Fun Sun Lemonade",
+        "price": 1.01
+    },
+    {
+        "id": 21,
+        "name": "Jelly Java",
+        "price": 2.81
+    },
+    {
+        "id": 22,
+        "name": "Never Enough Buts",
+        "price": 4.00
+    },
+    {
+        "id": 23,
+        "name": "Elephant Pig",
+        "price": 3.65
+    },
+    {
+        "id": 24,
+        "name": "Kevin's Coffee",
+        "price": 4.30
+    },
+    {
+        "id": 25,
+        "name": "Krabby Patties",
+        "price": 2.30
+    },
+    {
+        "id": 26,
+        "name": "Pink Plankton Pina Colada",
+        "price": 9.45
+    },
+    {
+        "id": 27,
+        "name": "Chumtastic Hot Chocolate",
+        "price": 2.30
+    },
+    {
+        "id": 28,
+        "name": "Syndrome's Super Chai",
+        "price": 7.40
+    },
+    {
+        "id": 29,
+        "name": "Omnidroid Light Roast",
+        "price": 3.80
+    },
+    {
+        "id": 30,
+        "name": "Zero Point Energy Shot",
+        "price": 3.80
+    },
+    {
+        "id": 31,
+        "name": "Sweet Rose Tea",
+        "price": 2.30
+    },
+    {
+        "id": 32,
+        "name": "Plant Potion",
+        "price": 2.33
+    },
+    {
+        "id": 33,
+        "name": "Pheromone Breve",
+        "price": 3.33
+    },
+    {
+        "id": 34,
+        "name": "Ada's Lemon Twist",
+        "price": 2.33
+    },
+    {
+        "id": 35,
+        "name": "Americano",
+        "price": 1.01
+    },
+    {
+        "id": 36,
+        "name": "Hot Chocolate",
+        "price": 2.81
+    },
+    {
+        "id": 37,
+        "name": "Water",
+        "price": 2.33
+    },
+    {
+        "id": 38,
+        "name": "Tea",
+        "price": 2.33
+    },
+    {
+        "id": 39,
+        "name": "Latte",
+        "price": 1.01
+    },
+    {
+        "id": 40,
+        "name": "Cappuccino",
+        "price": 2.81
+    }
+];
+
+module.exports = {
+  menuItems,
+};
